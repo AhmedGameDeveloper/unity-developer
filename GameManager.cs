679e7f51f2329a857e2b9be62d563b2da1e9028b
AhmedGameDeveloper <168006246+AhmedGameDeveloper@users.noreply.github.com>
2024-04-24 20:42:26 +0500

    Update GameManager.cs

--- GameManager.cs
+++ GameManager.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public Time f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+       SceneManager.LoadScene(1);
     }
 
     // Update is called once per frame
